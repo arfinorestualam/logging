6961a3559adf4a44df248636e248dbd65a8f6c4c
arfinorestualam <arfino.alam@gmail.com>
2024-11-13 09:22:19 +0700

    first

---
+++ src/test/java/pzn/springlogging/SpringLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package pzn.springlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
