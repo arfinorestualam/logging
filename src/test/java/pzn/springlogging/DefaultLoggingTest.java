1853f604806e03d280ac2d665071f48508482e77
arfinorestualam <arfino.alam@gmail.com>
2024-11-13 09:39:10 +0700

    learn logging level

--- src/test/java/pzn/springlogging/DefaultLoggingTest.java
+++ src/test/java/pzn/springlogging/DefaultLoggingTest.java
@@ -3,14 +3,19 @@ package pzn.springlogging;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @Slf4j
 @SpringBootTest
+//menambahkan properties baru untul belajar level
+@TestPropertySource("classpath:application-log-level.properties")
 public class DefaultLoggingTest {
 
     @Test
     void testDefaultLogging() {
         log.info("Hello World");
+        //setelah menggunakan properties yang sudah di set, log.info tidak keluar
+        //di karenakan di atur untuk folder pzn, level lognya warn ke atas, sehingga info tidak keluar
         log.warn("java logging");
         log.error("jav(a)");
 
